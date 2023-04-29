5b68a6cbb602dff3d2e68daf54bbdb9ec68520ef
Hexiang Qi <qihexiang@outlook.com>
2023-04-29 16:14:13 +0800

    Expandable map

---
+++ components/ExpandableIframe.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@mui/material";
+import { Close } from '@mui/icons-material';
+import { Box } from "@mui/system";
+import { useState } from "react";
+
+export default function ExpandableIframe(props: { buttonName: string, autoExpand: boolean } & React.IframeHTMLAttributes<HTMLIFrameElement>) {
+
+    const { buttonName, autoExpand, ...iframeProps } = props;
+    const [expand, setExpand] = useState(autoExpand);
+    return <Box>
+        {expand ? <>
+            <Button color="error" onClick={() => setExpand(false)}><Close></Close></Button>
+            <iframe {...iframeProps}></iframe>
+        </> : <Button color="primary" onClick={() => setExpand(true)}>{buttonName}</Button>}
+    </Box>
+}
