a571b227bdc6b52778b548e9e8866d43b3a12655
Hexiang Qi <qihexiang@outlook.com>
2023-04-29 09:49:50 +0800

    don't log non-cookie access

--- lib/redis.ts
+++ lib/redis.ts
@@ -3,9 +3,11 @@ import { createClient } from "redis";
 const redis = createClient();
 const conn = redis.connect();
 
-export async function getCount(nanoid: string) {
+export async function getCount(nanoid?: string) {
   await conn;
-  await redis.hSet("count", nanoid, new Date().getTime());
+  if (nanoid !== undefined) {
+    await redis.hSet("count", nanoid, new Date().getTime());
+  }
   const count = await redis.hLen("count");
   return count;
 }
--- pages/api/counter.ts
+++ pages/api/counter.ts
@@ -5,14 +5,12 @@ import { nanoid } from "nanoid";
 import { NextApiRequest, NextApiResponse } from "next";
 
 async function counterRoute(req: NextApiRequest, res: NextApiResponse) {
-  if (req.session.count === undefined) {
-    req.session.count = nanoid();
-    await req.session.save();
-  }
-
-  const count = await getCount(req.session.count);
-
   if (req.method === "GET") {
+    const count = await getCount(req.session.count);
+    if (req.session.count === undefined) {
+      req.session.count = nanoid();
+      await req.session.save();
+    }
     return res.json({
       count,
     });
