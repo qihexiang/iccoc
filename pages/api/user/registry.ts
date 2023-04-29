73261842a14d8d606d51ed20ee0395ffd57447b8
Hexiang Qi <qihexiang@outlook.com>
2023-04-29 10:14:42 +0800

    Add weak password catch on backend

--- pages/api/user/registry.ts
+++ pages/api/user/registry.ts
@@ -1,25 +1,39 @@
 import { errorLog } from "@/lib/errors";
 import { sessionOptions } from "@/lib/session";
 import { createUser } from "@/lib/user";
+import { passwordStrength } from "check-password-strength";
 import { withIronSessionApiRoute } from "iron-session/next";
 import { NextApiRequest, NextApiResponse } from "next";
+import { z } from "zod";
+
+const emailCheck = z.string().email();
 
 async function registryRoute(req: NextApiRequest, res: NextApiResponse) {
   try {
     const { email, password } = await req.body;
+    if (passwordStrength(password).id === 0) {
+      return res.status(400).json({
+        message: "Invalid password: Too weak.",
+      });
+    }
+    if (!emailCheck.safeParse(email).success) {
+      return res.status(400).json({
+        message: "Invalid given email: Not an email address",
+      });
+    }
     await createUser(email, password);
     req.session.user = {
       email,
     };
     await req.session.save();
-    res.status(200).json({
+    return res.status(200).json({
       user: {
         email,
       },
     });
   } catch (err) {
     errorLog(err);
-    res.status(500).json({
+    return res.status(500).json({
       message: "Failed to create account. Please check your email address.",
     });
   }
