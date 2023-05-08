c897d4c89cb8433a96befee246fb23d527e7bfcc
Hexiang Qi <qihexiang@outlook.com>
2023-05-09 01:00:41 +0800

    REDIS_URL

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,6 +1,8 @@
 import { createClient } from "redis";
 
-const redis = createClient();
+const redis = createClient({
+  url: process.env["REDIS_URL"]
+});
 const conn = redis.connect();
 
 export async function getCount(nanoid?: string) {
