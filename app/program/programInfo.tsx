a53da38cc489a299ad09a51dfbba9f5a730351bb
Hexiang Qi <qihexiang@outlook.com>
2023-07-19 09:15:29 +0800

    Move program to approuter

---
+++ app/program/programInfo.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { Container } from "@mui/material";
+import { Box } from "@mui/system";
+import Image from "next/image";
+import LectureTimetable from "@/images/LectureTimetable.png";
+
+export default function ProgramInfo() {
+  return (
+    <Container sx={{display: "flex", alignItems: "center", justifyContent: "center"}}>
+      <Box sx={{ maxWidth: "80%" }}>
+        <Image
+          style={{height: "100%", width: "100%"}}
+          src={LectureTimetable}
+          alt="program table"
+        ></Image>
+      </Box>
+    </Container>
+  );
+}
