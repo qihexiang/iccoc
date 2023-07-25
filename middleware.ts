8a6d98710b2a3b5a0d0b66016af456c37c42e7f0
Hexiang Qi <qihexiang@outlook.com>
2023-07-25 17:37:16 +0800

    Auto SSL redirect

---
+++ middleware.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from "next/server";
+import forwardedParse from "forwarded-parse";
+
+export const middleware = (req: NextRequest) => {
+  if (process.env["NODE_ENV"] === "production") {
+    const forwarded = forwardedURL(req);
+    const origin =
+      forwarded !== null ? new URL(req.nextUrl, forwarded) : new URL(req.url);
+    if (origin.protocol === "https:") {
+      return NextResponse.next();
+    }
+    origin.protocol = "https:";
+    return NextResponse.redirect(origin);
+  }
+
+  return NextResponse.next();
+};
+
+const forwardedURL = (req: NextRequest) => {
+  const forwarded = req.headers.get("Fowarded");
+  if (forwarded !== null) {
+    const parsed = forwardedParse(forwarded)[0];
+    return `${parsed.proto}://${parsed.host}`;
+  }
+
+  return null;
+};
