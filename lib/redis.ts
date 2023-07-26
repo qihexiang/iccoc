10dbe32cd611032e5c0980ec01fc05513257dd90
Hexiang Qi <qihexiang@outlook.com>
2023-07-26 11:13:22 +0800

    Temporily use non-id counter

--- lib/redis.ts
+++ lib/redis.ts
@@ -8,8 +8,10 @@ const conn = redis.connect();
 export async function getCount(nanoid?: string) {
   await conn;
   if (nanoid !== undefined) {
+    // await redis.incr("counter")
     await redis.hSet("count", nanoid, new Date().getTime());
   }
-  const count = await redis.hLen("count");
+  // const count = await redis.hLen("count");
+  const count = await redis.incr("counter") + 1976
   return count;
 }
