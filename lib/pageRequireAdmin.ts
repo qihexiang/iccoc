78271d9b4d1c31b0b536f49b8eb935ae2dc9f449
Hexiang Qi <qihexiang@outlook.com>
2023-08-19 18:38:02 +0800

    Rename default function to pageRequireAdmin

--- lib/pageRequireAdmin.ts
+++ lib/pageRequireAdmin.ts
@@ -3,7 +3,7 @@ import { redirect } from "next/navigation";
 import { parseToken } from "./cookieConfig";
 import prisma from "./prisma";
 
-export default async function apiRequireAdmin() {
+export default async function pageRequireAdmin() {
   const username = parseToken(cookies().get("admin")?.value)
   if (username === null) return redirect("/admin/login")
   if ((await prisma.admin.count({ where: { username } })) === 0) return redirect("/admin/login")
