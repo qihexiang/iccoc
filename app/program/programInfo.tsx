4d756e5b04c2f5f3b0eb822d889675250e85db26
Hexiang Qi <qihexiang@outlook.com>
2023-08-20 10:05:30 +0800

    New timetable

--- app/program/programInfo.tsx
+++ app/program/programInfo.tsx
@@ -3,7 +3,7 @@
 import { Button, Container } from "@mui/material";
 import { Box } from "@mui/system";
 import Image from "next/image";
-import LectureTimetable from "@/images/LectureTimetable.png";
+import LectureTimetable from "@/images/LecturesTimetable.png";
 import NextLink from "next/link";
 
 export default function ProgramInfo() {
@@ -11,9 +11,9 @@ export default function ProgramInfo() {
     <Container
       sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
     >
-      <Box sx={{ maxWidth: "80%" }}>
+      <Box sx={{padding: 1}}>
         <Image
-          style={{ height: "100%", width: "100%" }}
+          style={{ height: "100%", width: "100%", border: "1px solid rgba(0,0,0,1)", boxShadow: "0 0 8px 8px rgba(0,0,0,0.1)" }}
           src={LectureTimetable}
           alt="program table"
         ></Image>
