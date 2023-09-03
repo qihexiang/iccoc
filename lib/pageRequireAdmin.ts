1dea8815b6b714f9ff090d165ef7aec3d192f631
Hexiang Qi <qihexiang@outlook.com>
2023-09-03 19:22:39 +0800

    添加重定向状态

--- lib/pageRequireAdmin.ts
+++ lib/pageRequireAdmin.ts
@@ -5,7 +5,7 @@ import prisma from "./prisma";
 
 export default async function pageRequireAdmin() {
   const username = parseToken(cookies().get("admin")?.value)
-  if (username === null) return redirect("/admin/login")
-  if ((await prisma.admin.count({ where: { username } })) === 0) return redirect("/admin/login")
+  if (username === null) return redirect(`/admin/login?status=NotLogin&user=${username}`)
+  if ((await prisma.admin.count({ where: { username } })) === 0) return redirect(`/admin/login?status=NoSuchUser&user=${username}`)
   return username
 }
